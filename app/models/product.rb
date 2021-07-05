70b498c9b8eae2d10aef9622f205c0f17c739640
rmzNadir <dgzrz99@gmail.com>
2021-07-05 13:35:05 -0500

    :bug: Fixed platforms/languages product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,8 @@ class Product < ApplicationRecord
   has_many :product_has_platforms, dependent: :destroy
   has_many :platforms, through: :product_has_platforms
 
-  validates :platforms, :languages, presence: true, on: :update
-  validates_associated :platforms, :languages, presence: true, on: :update
+  validates :platforms, :languages, presence: true, on: :save
+  validates_associated :platforms, :languages, presence: true, on: :save
   validates :name, :price, :description, :stock, :provider, :shipping_cost, presence: true
   validates :has_free_shipping, inclusion: { in: [true, false] }
 
@@ -16,16 +16,18 @@ class Product < ApplicationRecord
   private
 
   def save_languages
+    return errors.add(:languages, I18n.t('activerecord.errors.models.product.attributes.languages.blank')) if @languages.nil?
+
     @languages.each do |language_id|
       ProductHasLanguage.create(language_id: language_id, product_id: id)
     end
   end
 
   def save_platforms
+    return errors.add(:languages, I18n.t('activerecord.errors.models.product.attributes.platforms.blank')) if @platforms.nil?
+
     @platforms.each do |platform_id|
       ProductHasPlatform.create(platform_id: platform_id, product_id: id)
     end
   end
-
- 
 end
