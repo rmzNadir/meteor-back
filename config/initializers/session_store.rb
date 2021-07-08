db221f7e083c22660abdbf366407b892e9afb5ba
rmzNadir <dgzrz99@gmail.com>
2021-07-08 18:54:16 -0500

    :construction:

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === "production"
-    Rails.application.config.session_store :cookie_store, key: "_meteor", domain: "https://meteor-erp-api.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_meteor", domain: "https://meteor-erp-app.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_meteor"
 end
