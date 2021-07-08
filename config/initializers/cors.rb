b6d11a36eec26e80153090239f5052b846ed682f
rmzNadir <dgzrz99@gmail.com>
2021-07-08 17:00:50 -0500

    :construction:

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,7 @@
+Rails.application.config.action_dispatch.cookies_same_site_protection = :none
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+
     allow do
         origins "http://localhost:3001"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
 if Rails.env === "production"
-    Rails.application.config.session_store :cookie_store, key: "_meteor", domain: "meteor-api.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_meteor", 
+    domain: nil,
+    secure: true,
+    httponly: false,
+    same_site: :none
 else
     Rails.application.config.session_store :cookie_store, key: "_meteor"
 end
