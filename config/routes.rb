2fa095e78b5727244d2834ab832253fce0cb955c
rmzNadir <dgzrz99@gmail.com>
2021-07-08 19:06:29 -0500

    bruh

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "static#home"
 
   namespace :api do
     resources :products
@@ -10,7 +11,6 @@ Rails.application.routes.draw do
     delete :logout, to: "sessions#logout"
     get :logged_in, to: "sessions#logged_in"
     resources :registrations, only: [:create]
-    root to: "static#home"
   end
   
 
