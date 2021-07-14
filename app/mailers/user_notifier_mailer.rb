1c29bc798fbbfd3e3e7628776237a9aa522c0276
rmzNadir <dgzrz99@gmail.com>
2021-07-13 19:05:16 -0500

    :construction: changed mail sender name

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,11 +1,13 @@
 class UserNotifierMailer < ApplicationMailer
-  default from: 'dgzrz99@gmail.com'
+  default from: 'Meteor <dgzrz99@gmail.com>'
 
   def send_sale_confirmation(user, sale)
     @user = user
     @sale = sale
     attachments.inline['logo.png'] = File.read("app/assets/images/meteor.png")
-    mail( to: @user.email,
-          subject: "Confirmación de orden No. #{@sale.id}" )
+    mail(
+      to: @user.email,
+      subject: "Confirmación de orden No. #{@sale.id}"
+    )
   end
 end
