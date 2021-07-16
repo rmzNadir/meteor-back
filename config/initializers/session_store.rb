fd5970ae3b5bef06089f0fbc7fe31377194b14c9
rmzNadir <dgzrz99@gmail.com>
2021-07-16 11:52:50 -0500

    :bug:

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,9 +1,10 @@
 if Rails.env === "production"
     Rails.application.config.session_store :cookie_store, key: "_meteor",
-    domain: '.meteor-erp-app.herokuapp.com'
+    domain: '.meteor-erp-app.herokuapp.com',
+    same_site: :strict
+
     # secure: true,
     # httponly: true,
-    # same_site: :none
 else
     Rails.application.config.session_store :cookie_store, key: "_meteor"
 end
